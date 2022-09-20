f777907aa0694e9f9aeae5d111357f20b59bdc09
markavilin1 <avilin1@outlook.com>
2022-09-20 18:36:04 -0300

    New Update

--- pay_system/settings.py
+++ pay_system/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifesticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifesticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
